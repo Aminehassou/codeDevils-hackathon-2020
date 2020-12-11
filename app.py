d116145747058372cd782918d4dc75fd535ad821
ahmed japnoo <japnoo@gmail.com>
2020-12-11 23:27:11 +0100

    Created first functional bot command

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+# Use the package we installed
+import logging
+from slack_bolt import App, BoltContext
+from slack_sdk import WebClient
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
---
+++ init.py
@@ -0,0 +1,5 @@
+from app import app
+from commands import hello
+# Start your app
+if __name__ == "__main__":
+    app.start(port=3000)
